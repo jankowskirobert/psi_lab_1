820283f813c00b54b698f88e36099eb6a34258c2
Jankowski Robert <jankowski.r@hotmail.com>
2017-04-21 14:32:48 +0200

    add BEST :) nd4j - math matrix lib to pom.xml

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -11,6 +11,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
 import gui.commands.AddCityPointCommand;
 import gui.commands.GraphicCommand;
 
@@ -34,6 +37,19 @@ public class Window extends JFrame {
 	}
 
 	public static void main(String[] args) {
+		  INDArray nd = Nd4j.create(new float[]{1,2},new int[]{1, 2}); //vector as row
+	        INDArray nd2 = Nd4j.create(new float[]{3,4},new int[]{2, 1}); //vector as column
+	        INDArray nd3 = Nd4j.create(new float[]{1,3,2,4},new int[]{2,2}); //elements arranged column major
+	        INDArray nd4 = Nd4j.create(new float[]{3,4,5,7,6,6},new int[]{3, 2});
+
+	        System.out.println(nd);
+	        System.out.println(nd2);
+	        System.out.println(nd3);
+	        System.out.println(nd4);
+	        INDArray ndv = nd.mmul(nd2);
+
+	        System.out.println(ndv);
+
 		new Window();
 	}
 
